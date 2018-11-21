2e3147553133211f09242a3c69416c8054b02ec3
Wil Cook <wil.cook.is@gmail.com>
2018-11-21 16:16:23 +0000

    Done.

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,13 +1,4 @@
 require_relative "blackjack.rb"
 
-def runner
-  welcome
-  hand = initial_round
-  until hand > 21
-    hand = hit?(hand)
-    display_card_total(hand)
-  end
-  end_game(hand)
-end
 
 runner
